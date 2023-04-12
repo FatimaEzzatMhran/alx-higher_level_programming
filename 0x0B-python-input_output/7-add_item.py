42b6914f7eb4ae8ea5dfa23e25a25f6567f43328
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-04-12 22:26:44 +0000

    7-add_item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""adds all arguments to a Python list and then saves them to a file."""
+import sys
+
+if __name__ = "__main__":
+    save_to_json_file = __import('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+                  __import__('6-load_from_json_file').load_from_json_file
+
+    filename = "add_item.json"
+
+    try:
+        items_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        items_list = []
+
+    for arg in argv[1:]:
+        items_list.append(arg)
+
+    save_to_json_file(items_list, filename)

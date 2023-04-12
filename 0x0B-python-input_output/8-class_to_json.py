ad28708fddf641bdf8d11de7d48a5eb36279dc1d
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-04-12 22:50:14 +0000

    8-class_to_json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+"""Contains a function called class_to_json."""
+
+
+def class_to_json(obj):
+    """Returns the dic representation of a simple data structure."""
+    return (obj.__dict__)

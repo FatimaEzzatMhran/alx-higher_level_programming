f8a05d27306753d323a341758cd140ab7d5a0926
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-04-12 20:52:56 +0000

    1-write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,5 +12,4 @@ def write_file(filename="", text=""):
         The number of chars writtem.
     """
     with open(filename, "w", encoding="utf-8") as f:
-        chars_written = f.write(text)
-        return (chars_written)
+        return (f.write(text))

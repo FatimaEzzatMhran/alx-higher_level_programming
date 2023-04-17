200eb747b404d429b7a72870ed02c57883226565
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-04-17 07:55:00 +0000

    tests

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,3 +45,27 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set."""
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances."""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r', encoding='utf-8') as f:
+                list_dict = Base.from_json_string(f.read())
+                return [cls.create(**obj) for obj in list_dict]
+        except IOError:
+            return []
+
+

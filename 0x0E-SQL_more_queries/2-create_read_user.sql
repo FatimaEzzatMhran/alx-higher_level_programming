942c26b1a9ddf1c6b745e2d78fd72b062659fddc
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-05-17 16:59:50 -0700

    create read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- create database hbtn_0d_2 and if it exists script should not fail
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- create user user_0d_2 and set password to user_0d_2_pwd
 -- If user exists, script should not fail
@@ -7,5 +7,4 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 
 -- user_0d_2 shold have only SELECT privileges in the database hbtn_0d_2
-GRANT SELECT ON `hbtb_0d_2`.*
-TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtb_0d_2.* TO 'user_0d_2'@'localhost'

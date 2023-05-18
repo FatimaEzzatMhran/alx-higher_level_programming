193dbdc8cc6daa18a91c63c11b2ca186c65a8509
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-05-17 18:02:19 -0700

    genre id

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,6 +4,6 @@
 -- only one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
-LEFT JOIN tv_shows 
+RIGHT JOIN tv_shows 
 ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;

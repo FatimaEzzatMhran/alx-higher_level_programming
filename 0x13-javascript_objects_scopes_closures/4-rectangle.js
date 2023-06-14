832fb2e5ad5d6ffb9864619f3bf0ad0b41aa4a52
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-06-14 00:20:59 +0000

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -12,5 +12,17 @@ class Rectangle {
       console.log(('X').repeat(this.width));
     }
   }
+
+  rotate () {
+    let temp = 0;
+    temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 module.exports = Rectangle;

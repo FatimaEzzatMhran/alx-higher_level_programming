5fbd6174ae96a9df698608709e59c00e7af4dd36
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-06-17 15:27:19 -0700

    select_states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!usr/bin/python3
+"""
+This script lists all states from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """Connect to MySQLdb"""
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         host='localhost',
+                         port=3306)
+
+    """Create a cursor object"""
+    cur = db.cursor()
+
+    """Execute SQL query -SELECT-"""
+    cur.exuecute("SELECT * FROM states ORDER BY id ASC")
+
+    """Fetch all rows and print them"""
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    """Close database cursor and database connection"""
+    cur.close()
+    db.close()

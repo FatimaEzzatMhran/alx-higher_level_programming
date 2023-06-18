602d93c82c4113f18a42be47127777985cdcc449
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-06-18 15:47:06 -0700

    14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """
-This script prints all City objects
-from the database `hbtn_0e_14_usa`.
+This script prints all City objects from the database hbtn_0e_14_usa
 """
 
-from sys import argv
+
+import sys 
 from model_state import State, Base
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the cities
-    from the database.
-    """
+    """Access to the db to get the states"""
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+            .format(sys.argv[1], sys.argv[2], sys.argv[3])
 
     engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)

e7a389356e4061b7b410631c49712be91456bfc7
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-06-18 14:30:54 -0700

    model_city and 14-model_city_ftch

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This script prints all City objects from the database hbtn_0e_14_usa
+"""
+
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    """Access to the db to get the states"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    """Create a session"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """Query the database to retrieve all City objects"""
+    cities = session.query(State, City).filter(State.id == City.state_id)\
+            .order_by(City.id.asc())
+
+    """Print the results"""
+    for city, state in cities.all():
+        print(f"{state.name}: ({city.id}) {City.name}")
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script contains the class definition of a City
+and an instance Base = declarative_base().
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from model_state import Base, State
+
+class City(Base):
+    """
+    City class has the following attributes:
+    __tablename__: the table name of the class
+    id: The state id of class City - 
+        Integer, auto-generated, unique, not nullable
+    name: The city name of class City -
+          String, 128, not nullable
+    state_id: The state id -
+              Integer, foreign key, not nullable
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)

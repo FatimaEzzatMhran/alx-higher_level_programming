fb862a7fbc3df10538d1db9bad603fcb84ea3a72
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-06-18 15:47:44 -0700

    14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
             .format(sys.argv[1], sys.argv[2], sys.argv[3])
 
-    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
 
     session = Session()

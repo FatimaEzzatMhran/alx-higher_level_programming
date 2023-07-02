951cd3d5258e7f0f6d752f53c0878d62ddac3eb0
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-07-02 16:27:29 -0700

    header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/puthon3
+"""
+Script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response.
+"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        url = sys.argv[1]
+
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as response:
+            headers = response.info()
+            x_request_id = headers.get('X-Request-Id')
+            if x_request_id:
+                print(x_request_id)

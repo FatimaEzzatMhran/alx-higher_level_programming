42a225477a6bc66e92629acb3b3648e803b84c97
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-07-02 18:28:20 -0700

    errpr

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+"""
+Python script that takes in a URL
+Sends a request to the URL and displays the body of the response
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        url = sys.argv[1]
+
+        r = requests.get(url)
+        if r.status_code >= 400:
+            print("Error code: {}".format(r.status_code))
+        else:
+            print(response.text)
